919e91a015afa42be148fcf385ff608882744227
Unknown <jquintanamartin@hotmail.com>
2018-07-28 14:47:31 -0300

    Random Spawns Over Time

--- Frogger/Assets/Scripts/SpawnPoint.cs
+++ Frogger/Assets/Scripts/SpawnPoint.cs
@@ -6,8 +6,23 @@ public class SpawnPoint : MonoBehaviour {
 
     public GameObject car;
     public bool flip = false;
+    public float minTime = 3;
+    public float maxTime = 5;
+
+    private float time;
+    private float rand;
 
 	void Start () {
+        Instanciate();
+        rand = Random.Range(minTime, maxTime);
+	}
+
+    private void Update()
+    {
+        InstanciateInSeconds();
+    }
+
+    void Instanciate() {
         if (flip)
         {
             car.GetComponent<SpriteRenderer>().flipX = true;
@@ -20,7 +35,13 @@ public class SpawnPoint : MonoBehaviour {
             car.GetComponent<Car>().direction = Vector2.right;
             Instantiate(car, transform.position, Quaternion.identity);
         }
-        
-	}
-	
+    }
+    void InstanciateInSeconds() {
+        time += Time.deltaTime;
+        if (time >= rand)
+        {
+            Instanciate();
+            time = 0;
+        }
+    }
 }
