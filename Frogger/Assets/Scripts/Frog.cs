21b5acf0b6dcd948990d4c03e5a5d0e5493893a6
Unknown <jquintanamartin@hotmail.com>
2018-07-27 22:05:05 -0300

    Bounding Box

--- Frogger/Assets/Scripts/Frog.cs
+++ Frogger/Assets/Scripts/Frog.cs
@@ -7,7 +7,22 @@ public class Frog : MonoBehaviour {
     public Rigidbody2D rb;
     public float movementQuantity;
 
-	void Update () {
+    Vector3 wrld;
+    float half_sz;
+
+    private void Start()
+    {
+        wrld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, 0.0f));
+        half_sz = gameObject.GetComponent<Renderer>().bounds.size.x / 2;
+    }
+   
+
+    void Update () {
+        Movement();
+        Bounds();
+    }
+
+    void Movement() {
         if (Input.GetKeyDown(KeyCode.UpArrow))
             rb.MovePosition(rb.position + (Vector2.up * movementQuantity));
         else if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -16,5 +31,15 @@ public class Frog : MonoBehaviour {
             rb.MovePosition(rb.position + (Vector2.left * movementQuantity));
         else if (Input.GetKeyDown(KeyCode.RightArrow))
             rb.MovePosition(rb.position + (Vector2.right * movementQuantity));
+    }
+    void Bounds() {
+        if (rb.position.x >= (wrld.x - half_sz))
+        {
+            rb.MovePosition(rb.position + (Vector2.left * movementQuantity));
+        }
+        else if (rb.position.x <= (half_sz - wrld.x))
+        {
+            rb.MovePosition(rb.position + (Vector2.right * movementQuantity));
+        }
     }
 }
