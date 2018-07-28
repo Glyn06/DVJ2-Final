0e85e7b32997e320b575bf4bbe71fe8a19afe8f2
Unknown <jquintanamartin@hotmail.com>
2018-07-28 14:34:28 -0300

    Car Movement

---
+++ Frogger/Assets/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed = 0.1f;
+    [HideInInspector]
+    public Vector2 direction;
+
+    private void LateUpdate()
+    {
+        Move(direction);
+    }
+
+    public void Move(Vector2 dir) {
+        rb.MovePosition(rb.position + (dir * speed));
+    }
+}
--- Frogger/Assets/Scripts/SpawnPoint.cs
+++ Frogger/Assets/Scripts/SpawnPoint.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class SpawnPoint : MonoBehaviour {
 
     public GameObject car;
+    public bool flip = false;
 
 	void Start () {
-        Instantiate(car, transform);
+        if (flip)
+        {
+            car.GetComponent<SpriteRenderer>().flipX = true;
+            car.GetComponent<Car>().direction = Vector2.left;
+            Instantiate(car, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            car.GetComponent<SpriteRenderer>().flipX = false;
+            car.GetComponent<Car>().direction = Vector2.right;
+            Instantiate(car, transform.position, Quaternion.identity);
+        }
+        
 	}
 	
 }
