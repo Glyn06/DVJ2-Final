3c8c087f32182a556516bc4c7f1d3e3e19f1fe67
Unknown <jquintanamartin@hotmail.com>
2018-07-28 13:57:01 -0300

    Car Spawns

---
+++ Frogger/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject car;
+
+	void Start () {
+        Instantiate(car, transform);
+	}
+	
+}
