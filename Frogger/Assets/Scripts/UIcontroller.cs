99ede3ae646c9e61748676a0524a90e730276581
Unknown <jquintanamartin@hotmail.com>
2018-08-04 13:48:58 -0300

    InGame HUD

---
+++ Frogger/Assets/Scripts/UIcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIcontroller : MonoBehaviour {
+
+    public Text livesText;
+    public Text scoreText;
+    public Text timeText;
+
+    public float seconds;
+    public float minutes;
+
+    private void Update()
+    {
+        seconds = (int)(Time.unscaledTime % 60f);
+        minutes = (int)(Time.unscaledTime / 60f);
+        timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+        livesText.text = Frog.instance.lives.ToString("000");
+    }
+}
